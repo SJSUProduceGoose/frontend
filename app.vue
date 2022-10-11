26818b535486edeb455c95ddb28315a3797bd0f0
Vincent Chen <vchen14789@gmail.com>
2022-10-11 16:59:00 -0700

    Added goose image

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      url: 'goose.png'
+    }
+  }
+}
+</script>
+
 <template>
-  <div>
-   
-  </div>
+  <img :src=url>
 </template>
+
+<style scoped>
+img {
+  width: 400px;
+  height: 600px;
+  position: fixed;
+  top: 150px;
+  left: 450px;
+  
+
+}
+</style>
