821ac5c2365bdc6d7e91b7851431c4f6cf5456bf
NoahCardoza <noahcardoza@gmail.com>
2022-11-02 23:01:40 -0700

    WEB-14: convert login/registration to el-plus

--- pages/login.vue
+++ pages/login.vue
@@ -1,18 +1,25 @@
 <script setup>
+import { ElButton, ElInput, ElForm, ElFormItem, ElLink, ElAlert } from 'element-plus'
 import { useUserStore } from "@/store/user";
 
-const username = ref('jeff.bezos@sjsu.edu')
-const password = ref('superuser')
+const showError = ref(false)
+const errorText = ref('')
+
+const form = reactive({
+  username: 'jeff.bezos@sjsu.edu',
+  password: 'superuser',
+})
 const userStore = useUserStore()
 const router = useRouter()
 
 async function onSubmit(e) {
+  console.log(e);
   e.preventDefault();
 
   const formData = new FormData()
   
-  formData.append('username', username.value)
-  formData.append('password', password.value)
+  formData.append('username', form.username)
+  formData.append('password', form.password)
 
   try {
     const response = await useApi('/auth/token/', {
@@ -22,34 +29,42 @@ async function onSubmit(e) {
     userStore.setWithUserToken(response.access_token)
     router.push('/shop')
   } catch (error) {
-    // TODO: nicer notifications
-    alert(error.data.detail);
+    showError.value = true
+    errorText.value = error?.data?.detail || 'An error occurred while logging in.'
   }
 }
+
 </script>
 
 <template>
-  <div class="container">
-    <form class="card p-4" @submit="onSubmit">
-        <h3 class="mb-4">Login</h3>
-        <div class="mb-3">
-          <label for="username" class="form-label">Username</label>
-          <input v-model="username" autofocus type="text" class="form-control" id="username" name="username">
+  <div class="auth-container">
+    <el-form :model="form" label-width="90px" class="card auth-form-card">
+      <h3 class="mb-4">Login</h3>
+      <el-form-item label="Username">
+        <el-input v-model="form.username"/>
+      </el-form-item>
+      <el-form-item label="Password">
+        <el-input v-model="form.password" type="password" autocomplete="off" />
+      </el-form-item>
+      <el-form-item>
+        <el-alert
+          v-if="showError"
+          type="error"
+          title="Login Error"
+          :description="errorText"
+          show-icon
+          :closable="false"
+        />
+      </el-form-item>
+      <el-form-item>
+        <div style="justify-content: space-between;width:100%;display: flex;align-items: center;">
+          <el-button type="primary" @click="onSubmit">Submit</el-button>
+          <router-link custom to="/register" v-slot="{ navigate, href }">
+            <el-link type="info" :href="href" @click="navigate" style="line-height: initial;">Register</el-link>
+          </router-link>
         </div>
-        <div class="mb-3">
-          <label for="password" class="form-label">Password</label>
-          <input v-model="password" type="password" class="form-control" id="password" name="password" aria-describedby="password">
-        </div>
-        <button type="submit" class="btn btn-primary mb-3 w-100">Submit</button>
-        <small>
-          <div class="text-center">
-            New here?
-            <NuxtLink href="/register" class="link-secondary">
-              Register
-            </NuxtLink>
-          </div>
-        </small>
-      </form>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -63,14 +78,5 @@ async function onSubmit(e) {
 .card {
   width: 90%;
   max-width: 500px;
-}
-
-.btn-primary {
-  background-color: rgb(23, 162, 255);
-  border-color: rgb(23, 162, 255);
-}
-.btn-primary:hover {
-  background-color: rgb(0 153 255);
-  border-color: rgb(0 153 255);
 }
 </style>
--- pages/register.vue
+++ pages/register.vue
@@ -1,92 +1,152 @@
-<script setup>
+<script setup lang="ts">
+import { ElButton, ElInput, ElForm, ElFormItem, ElLink, ElAlert, FormInstance } from 'element-plus'
 import { useUserStore } from "@/store/user";
 
-const firstname = ref('Test')
-const lastname = ref('User')
-const username = ref('test.user@sjsu.edu')
-const password = ref('testuser')
+const showError = ref<boolean>(false)
+const errorText = ref<string>('')
+
+const formRef = ref<FormInstance>()
+
+const form = reactive({
+  firstname: 'Test',
+  lastname: 'User',
+  username: 'test.user@sjsu.edu',
+  password: 'testuser',
+  confirmPassword: 'testuser',
+})
 
 const userStore = useUserStore()
 const router = useRouter()
 
-async function onSubmit(e) {
+async function onSubmit(e: Event) {
   e.preventDefault();
+  
+  formRef.value?.validate(async (valid: boolean) => {
+    if (!valid) {
+      return false;
+    }
 
-  const formData = new FormData()
+    const formData = new FormData()
 
-  // TODO: json or form data?
-  formData.append('firstname', firstname.value)
-  formData.append('lastname', lastname.value)
-  formData.append('username', username.value)
-  formData.append('password', password.value)
+    // TODO: json or form data?
+    formData.append('firstname', form.firstname)
+    formData.append('lastname', form.lastname)
+    formData.append('username', form.username)
+    formData.append('password', form.password)
 
-  try {
-    // TODO: verify the endpoint url
-    const response = await useApi('/auth/register/', {
-      method: 'POST',
-      body: formData
-    })
-    userStore.setWithUserToken(response.access_token)
-    router.push('/shop')
-  } catch (error) {
-    // TODO: nicer notifications
-    alert(error.data.detail);
+    try {
+      // TODO: verify the endpoint url
+      const response: any = await useApi('/auth/register/', {
+        method: 'POST',
+        body: formData
+      })
+      userStore.setWithUserToken(response.access_token)
+      router.push('/shop')
+    } catch (error: any) {
+      showError.value = true
+      errorText.value = error?.data?.detail || 'An error occurred while registering.';      
+    }
+
+  })
+}
+
+const validatePass = (rule: any, value: any, callback: any) => {
+  if (value === '') {
+    callback(new Error('Please input the password'))
+  } else {
+    if (form.confirmPassword !== '') {
+      if (!formRef.value) return
+      formRef.value.validateField('confirmPassword', () => null)
+    }
+    callback()
   }
 }
+const validatePass2 = (rule: any, value: any, callback: any) => {
+  if (value === '') {
+    callback(new Error('Please input the password again'))
+  } else if (value !== form.password) {
+    callback(new Error("The two passwords do no match!"))
+  } else {
+    callback()
+  }
+}
+
+const rules = reactive({
+  firstname: [{required: true}],
+  lastname: [{ required: true }],
+  username: [
+    {
+      required: true,
+      type: 'email',
+      message: 'Please input a valid email address',
+      trigger: 'blur'
+    },
+  ],
+  password: [
+    {
+      required: true, validator: validatePass, trigger: 'blur',
+    },
+    {
+      type: 'string', min: 8, message: 'Password must be at least 8 characters', trigger: 'blur',
+    },
+    {
+      type: 'string', max: 32, message: 'Password must be at most 32 characters', trigger: 'blur',
+    },
+  ],
+  confirmPassword: [{ required: true, validator: validatePass2, trigger: 'blur' }],
+})
+
+
 </script>
 
 <template>
-  <div class="container">
-    <form class="card p-4" @submit="onSubmit">
-        <h3 class="mb-4">Register</h3>
-        <div class="mb-3">
-          <label autofocus for="firstname" class="form-label">First Name</label>
-          <input type="text" disabled class="form-control" id="firstname" name="firstname" aria-describedby="firstnameHelp">
+  <div class="auth-container">
+    <el-form
+    class="card auth-form-card"
+    ref="formRef"
+    :model="form"
+    status-icon
+    :rules="rules"
+    label-width="90px"
+  >
+    <h3 class="mb-4">Register</h3>
+    <el-form-item label="First name" prop="firstname">
+      <el-input v-model="form.firstname"/>
+    </el-form-item>
+    <el-form-item label="Last name" prop="lastname">
+      <el-input v-model="form.lastname"/>
+    </el-form-item>
+    <el-form-item label="Email" prop="username">
+      <el-input v-model="form.username"/>
+    </el-form-item>
+    <el-form-item label="Password" prop="password">
+      <el-input v-model="form.password" type="password" autocomplete="off" />
+    </el-form-item>
+    <el-form-item label="Confirm" prop="confirmPassword">
+      <el-input
+        v-model="form.confirmPassword"
+        type="password"
+        autocomplete="off"
+      />
+    </el-form-item>
+    <el-form-item>
+        <el-alert
+          v-if="showError"
+          type="error"
+          title="Registration Error"
+          :description="errorText"
+          show-icon
+          :closable="false"
+        />
+      </el-form-item>
+    <el-form-item>
+      <div style="justify-content: space-between;width:100%;display: flex;align-items: center;">
+          <el-button type="primary" @click="onSubmit">Submit</el-button>
+          <router-link custom to="/login" v-slot="{ navigate, href }">
+            <el-link type="info" :href="href" @click="navigate" style="line-height: initial;">Login</el-link>
+          </router-link>
         </div>
-        <div class="mb-3">
-          <label autofocus for="lastname" class="form-label">Last Name</label>
-          <input type="text" disabled class="form-control" id="lastname" name="lastname" aria-describedby="lastnameHelp">
-        </div>
-        <div class="mb-3">
-          <label autofocus for="email" class="form-label">Email</label>
-          <input type="text" disabled class="form-control" id="email" name="email" aria-describedby="emailHelp">
-          <div id="emailHelp" class="form-text">Must be a valid email you have access to.</div>
-        </div>
-        <div class="mb-3">
-          <label for="password" class="form-label">Password</label>
-          <input type="password" disabled  class="form-control" id="password" name="password" aria-describedby="passwordHelp">
-          <div id="passwordHelp" class="form-text">Must be between 8 and 255 characters inclusively.</div>
-        </div>
-        <button type="submit" class="btn btn-primary mb-3 w-100">Submit</button>
-        <small>
-          <div class="text-center fs-6">
-            Already have an accout?
-            <NuxtLink href="/login" class="link-secondary">
-              Login
-            </NuxtLink>
-          </div>
-        </small>
-      </form>
+    </el-form-item>
+  </el-form>
   </div>
 </template>
-
-<style>
-.container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-}
-.card {
-  width: 90%;
-  max-width: 500px;
-}
-.btn-primary {
-  background-color: rgb(23, 162, 255);
-  border-color: rgb(23, 162, 255);
-}
-.btn-primary:hover {
-  background-color: rgb(0 153 255);
-  border-color: rgb(0 153 255);
-}
-</style>
