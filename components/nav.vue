14aef568614c73f8bc6c762e382158f7ae77a7a2
NoahCardoza <noahcardoza@gmail.com>
2022-11-02 21:45:04 -0700

    WEB-14: convert navigation to el-plus

--- components/nav.vue
+++ components/nav.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ElButton, ElInput, ElIcon } from 'element-plus'
+import { ElButton, ElInput, ElIcon, ElMenu, ElMenuItem, } from 'element-plus'
 import { Search } from '@element-plus/icons-vue'
 import { useUserStore } from "@/store/user";
 
@@ -12,111 +12,41 @@ function navigateToSearch() {
 }
 
 const userStore = useUserStore()
-
-
 </script>
+
 <template>
-  <div class="header">
-    <div class="title">
-      <a class="reglink" href="/">
-        OFS Farms
-      </a>
-    </div>
-    <div class="hlinks">
-      <ul>
-        <li class="hlink-item">
-          <NuxtLink to="/shop" class="reglink">
-            Shop
-          </NuxtLink>
-        </li>
-        <li class="hlink-item" >
-          <NuxtLink v-if="userStore.user === null" to="/login" class="reglink">
-             Login
-          </NuxtLink>
-          <NuxtLink v-else="userStore.user === null" to="/account" class="reglink">
-             Account
-          </NuxtLink>
-        </li>
-      
-        <li>
-          <el-input
-            size="large"
-            v-model="query"
-            placeholder="Search"
-          >
-            <template #append>
-              <el-button
-                @click="navigateToSearch"
-                :style="{
-                  borderBottomLeftRadius: '0',
-                  borderTopLeftRadius: '0',
-                  height: '100%',
-                  backgroundColor: 'var(--pg-color-primary)',
-                }"
-              ><el-icon class="el-icon--center" color="white"><Search/></el-icon></el-button>
-            </template>
-          </el-input>
-        </li>   
-      </ul>
-    </div>
-  </div>
+    <el-menu
+      style="width: 100%;flex-direction: space-between;"
+      :default-active="activeIndex"
+      mode="horizontal"
+      router
+    >
+      <div style="padding: 0 1rem; display: flex; align-items: center;">
+        <h3>OFS Farms</h3>
+      </div> 
+      <el-menu-item index="/shop">Shop</el-menu-item>
+      <el-menu-item v-if="userStore.user === null" index="/login">Login</el-menu-item>
+      <el-menu-item v-else index="/account">Account</el-menu-item>
+      <div style="display: flex; align-items: center;padding: 0 1rem;">
+        <div>
+        <el-input
+          style="max-width: px;"
+          v-model="query"
+          placeholder="Search"
+        >
+          <template #append>
+            <el-button
+              @click="navigateToSearch"
+              :style="{
+                borderBottomLeftRadius: '0',
+                borderTopLeftRadius: '0',
+                height: '100%',
+                backgroundColor: 'var(--pg-color-primary)',
+              }"
+            ><el-icon class="el-icon--center" color="white"><Search/></el-icon></el-button>
+          </template>
+        </el-input>
+        </div>
+      </div>
+    </el-menu>
 </template>
-
-<style scoped>
-.header {
-  background-color: rgb(23, 162, 255);
-  display: flex;
-  position: sticky;
-  top: 0;
-  width: 100%;
-  z-index: 100;
-  min-width: 1400px;
-}
-
-.reglink {
-  cursor: default;
-  pointer-events: visible;        
-  text-decoration: none;
-  color: white;
-  transition: color 0.2s;
-}
-.reglink:hover {
-  color: var(--pg-color-primary);
-}
-.title {
-  font-size: 40px;
-  font-weight: 10px;
-  margin-left: 10px;
-}
-
-.hlinks {
-  margin-left: auto;
-  margin-right: 20px;
-}
-
-.hlink-item {
-  font-size: 25px;
-  margin-left: 10px;
-  margin-right: 20px;
-}
-.btn {
-  background-color: var(--pg-color-primary);
-  padding: 3px; 
-}
-
-.mcolor {
-  color:white;
-}
-
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  margin-top: 10px;
-}
-
-li {
-  display: inline-flex;
-}
-
-</style>
