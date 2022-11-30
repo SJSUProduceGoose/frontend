c618851f9e7e7affcea7740bfe45fb0655e3b205
Miggy Antonio <Mig.tonio11@gmail.com>
2022-11-29 23:30:12 -0800

    WEB-48, WEB-51: Fixed back button on account and search shows 'no search results found' when searching for a product that isn't in our database

--- pages/account.vue
+++ pages/account.vue
@@ -1,8 +1,6 @@
 <script setup>
 import { ElPageHeader, ElDescriptionsItem, ElDescriptions, ElAvatar, ElTag } from 'element-plus'
 
-const router = useRoute()
-
 definePageMeta({
   middleware: ['auth-customer']
 })
@@ -13,29 +11,23 @@ const { data } = await useApi('/user/me/')
   
 <template>
   <div class="content px-4">
-    <div aria-label="A complete example of page header">
-      <el-page-header @back="router.back()">
-
-        <template #content>
-          <div class="flex items-center">
-            <el-avatar class="mr-3" :size="100"
-              src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png" />
-            <span class="text-large font-600 mr-3"> {{ data.firstname }} {{ data.lastname }}
-            </span>
-          </div>
-        </template>
-
-        <el-descriptions title="Account Information" direction="vertical" :column="1" size="large" border>
-          <el-descriptions-item label="Full Name">{{ data.firstname }} {{ data.lastname }}
-          </el-descriptions-item>
-          <el-descriptions-item label="Email">{{ data.email }}</el-descriptions-item>
-          <el-descriptions-item label="Area" :span="4">San Jose</el-descriptions-item>
-          <el-descriptions-item label="Type">
-          <el-tag size="small">School</el-tag>
-          </el-descriptions-item>
-        </el-descriptions>
-      </el-page-header>
+    <div class="flex">
+      <PageHeader class="mt-3"/>
+      <div class="flex items-center">
+        <el-avatar class="mt-4 ml--13 mr-2" :size="50" src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png" />
+        <span class="text-large font-600 mr-3 mt-4"> {{ data.firstname }} {{ data.lastname }}
+        </span>
+      </div>
     </div>
+    <el-descriptions title="Account Information" direction="vertical" :column="1" size="large" border>
+      <el-descriptions-item label="Full Name">{{ data.firstname }} {{ data.lastname }}
+      </el-descriptions-item>
+      <el-descriptions-item label="Email">{{ data.email }}</el-descriptions-item>
+      <el-descriptions-item label="Area" :span="4">San Jose</el-descriptions-item>
+      <el-descriptions-item label="Type">
+        <el-tag size="small">School</el-tag>
+      </el-descriptions-item>
+    </el-descriptions>
   </div>
 </template>
   
