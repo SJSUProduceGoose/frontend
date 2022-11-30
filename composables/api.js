3f40e212ba5d6a1578541e2304ba8a90e2809870
NoahCardoza <noahcardoza@gmail.com>
2022-11-30 13:28:14 -0800

    WEB-53: fix error on return from stripe

--- composables/api.js
+++ composables/api.js
@@ -1,5 +1,35 @@
 import { useUserStore } from '@/store/user.js'
 
+export const $api = async (path, options) => {
+  const config = useRuntimeConfig();
+  const userStore = useUserStore();
+  const headers = useRequestHeaders(['cookie'])
+
+  try {
+    return await $fetch(path, {
+      credentials: 'include',
+      baseURL: config.BASE_URL || config.public.BASE_URL,
+      headers: {
+        ...headers,
+        ...(options.headers || {}),
+      },
+      ...options
+    })
+  } catch (error) {
+    if (error.response.status === 401) {
+      userStore.setupLoginNotification({
+        type: 'error',
+        title: 'Error',
+        message: error.data.detail,
+        durration: 2000,
+      });
+      navigateTo('/login')
+    }
+    throw error
+  }
+  
+}
+
 export const useApi = async (path, options={}) => {
   const config = useRuntimeConfig();
   const userStore = useUserStore();
